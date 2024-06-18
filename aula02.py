619abba4c78ba4081dd5bac4b8a75f813802608f
Gleiciele <gleicielerodrigues57@gmail.com>
2024-06-18 19:57:28 -0300

    estudando-modulo1

---
+++ aula02.py
@@ -0,0 +1,4 @@
+# A função print serve para imprimir e mostrar na tela e ela recebe argumentos
+print(12, 34, sep='-', end='\r\n')
+print(56, 78, sep="-", end='\n')
+print(9, 10, sep="-", end='\n')
---
+++ aula03.py
@@ -0,0 +1,20 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+string são textos que estão dentro de aspas
+"""
+print(1234)
+# Aspas simples
+print('Gleiciele Rodrigues')
+print(1, 'Gleiciele "Rodrigues"')
+
+# Aspas duplas
+print("Gleiciele Rodrigues")
+
+# Escape
+print('Gleiciele \"Rodrigues\"')
+
+# r 
+print(r'Gleiciele \"Rodrigues\"')
