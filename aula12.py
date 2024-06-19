2f8dd3abce208dd3c8592cbaea9dc8779cc5dc83
Gleiciele <gleicielerodrigues57@gmail.com>
2024-06-19 17:25:05 -0300

    aula-12

---
+++ aula12.py
@@ -0,0 +1,14 @@
+# 1. (n + n) (# a primeira coisa que será executada é oque
+# estar nos parenteses.)
+
+# 2. **  (# em segundo lugar a mas forte é a exponenciação.)
+
+# 3. * / // %  (# em terceiro lugar as divisões, multiplicações e modulo ou resto)
+
+# 4. + -  (# por ultimo adição e subtração.)
+
+conta_1 = (1 + 1) ** (5 + 5)
+print(conta_1)
+
+conta_2 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_2)
