efa89479b93ff4e8cd9bed2dd8593d062be047ea
Gleiciele <gleicielerodrigues57@gmail.com>
2024-06-19 12:46:34 -0300

    atualizando repositorio

--- aula08.py
+++ aula08.py
@@ -9,3 +9,10 @@ soma_dois_mais_dois = 2 + 2
 int_um = bool('1')
 print(int_um, type(int_um))
 print(nome_completo, soma_dois_mais_dois)
+
+
+nome = 'Gleice'
+idade = 26 
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade) 
+print('maior_de_idade?', maior_de_idade)
