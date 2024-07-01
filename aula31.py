14438f18734632e259e8f507c675c7b084e05489
Gleiciele <gleicielerodrigues57@gmail.com>
2024-07-01 15:00:30 -0300

    aula-3'

---
+++ aula31.py
@@ -0,0 +1,8 @@
+"""
+https:docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'gleiciele Rodrigues'
+#outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(string.zfill(30))
+print(string.capitalize())
